d108f304902edf1f8354808cb9677c03d750c50c
VINAGHOST <nguyentrungnhan87@gmail.com>
2024-07-02 16:45:36 +0700

    Add project files.

---
+++ Docker.Net/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+var configuration = builder.Build();
+
+var greeting = configuration.GetValue(typeof(string), "AppSettings:Greeting")?.ToString();
+
+var myConnectString = configuration.GetConnectionString("MyConnectionString");
+
+Console.WriteLine(greeting);
+Console.WriteLine(myConnectString);
